fc086cfd01470b8de8da4b27a2ad0931b93e8fdf
Max Eduard von Wedemeyer <max-eduard@t-online.de>
2021-08-24 17:21:22 +0200

    rest-show

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -2,6 +2,7 @@ class RestaurantsController < ApplicationController
 
   def show
     @restaurant = Restaurant.find(params[:id])
+    @drinks = @restaurant.menu_items.where(category: "drinks")
+    @mains = @restaurant.menu_items.where(category: "mains")
   end
-
 end
