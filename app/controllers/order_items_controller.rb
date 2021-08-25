31fe11287cdb3ec50b1e32e4f5356a457e301f2a
Janus Vollmer <janus.vollmer@gmail.com>
2021-08-25 15:37:18 +0200

    Added restaurant_id as foreign key to orders table

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -1,8 +1,32 @@
 class OrderItemsController < ApplicationController
+  def index
+    @order_items = OrderItem.all
+  end
+
+  def show
+    @order_item = OrderItem.find(params[:id])
+  end
+
+  def new
+    @order_item = OrderItem.new
+    @order = Order.find(params[:order_id])
+  end
 
   def create
-    @order_items = OrderItems.new
-    @order_items.menu_item
+    @order_item = OrderItems.new(order_item_params)
+    @order_item.order = Order.find(params[:order_id])
+    @order_item.order.user_id = current_user
+    if @order_item.save
+      redirect_to order_path(@order_item.order)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def order_item_params
+    params.require(:order_item).permit(:menu_item_id, :quantity)
   end
 
 end
