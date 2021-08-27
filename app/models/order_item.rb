2c7557ec640be59bc68ed494aa278cbfb855f1d2
Max Eduard von Wedemeyer <max-eduard@t-online.de>
2021-08-27 14:52:17 +0200

    style resolved

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -1,5 +1,5 @@
 class OrderItem < ApplicationRecord
   belongs_to :order
   belongs_to :menu_item
-  validates :quantity, presence: true
+  validates :quantity, numericality: { greater_than_or_equal_to: 1 }
 end
