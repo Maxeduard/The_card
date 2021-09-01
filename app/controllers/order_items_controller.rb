76a0c3da93e9eb528982108655da9ffbb1e6e529
Max Eduard von Wedemeyer <max-eduard@t-online.de>
2021-09-01 12:03:25 +0200

    bug-fix

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -9,10 +9,10 @@ class OrderItemsController < ApplicationController
     if @present_order_item
       @present_order_item.quantity += 1
       @present_order_item.save
-      redirect_to restaurant_path(@order_item.order.restaurant, anchor: "menu_item#{@order_item.menu_item.id}")
+      redirect_to restaurant_path(@order_item.order.restaurant)
     else
       if @order_item.save
-        redirect_to restaurant_path(@order_item.order.restaurant, anchor: "menu_item#{@order_item.menu_item.id}")
+        redirect_to restaurant_path(@order_item.order.restaurant)
       else
         render 'restaurants/show'
       end
